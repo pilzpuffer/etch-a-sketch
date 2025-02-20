807c21c061bc67ed3b66dd6ff868bbda4e814d53
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-02-20 12:32:25 +0200

    Add most of the animation functionality for the mettaton's arms

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const sketchField = document.querySelector('#mett-a-sketch');
+const leftArm = document.querySelector('#left-arm');
+const rightArm = document.querySelector('#right-arm');
 
 let fieldSize = 16;
 
@@ -17,4 +19,43 @@ for (let i = 0; i < fieldSize; i++) {
     
 }
 
+function getNumber(element){
+    const initialCss = getComputedStyle(document.documentElement).getPropertyValue(element);
+    let prepare = initialCss.slice(0, -1);
+    let toNumber = parseFloat(prepare);
+    return toNumber;
+};
 
+function adjustValue(element, operation, amount){
+
+    let properNumber = getNumber(element);
+    
+    let result;
+    
+    if (operation === "reduce") {
+        result = properNumber - amount;
+    } else if (operation === "increase") {
+        result = properNumber + amount;
+    } else {
+        console.log("Invalid operation");
+        return;
+    }
+    
+    return `${result}%`;
+    }
+
+let jazzHands = setInterval(function () {
+
+   //i need to sligthly re-think the logic here - might be best to implement with setTimout instead?
+    if (getNumber('--left-hand-position') >= 1.9 && getNumber('--right-hand-position') >= -3.8) {
+        document.documentElement.style.setProperty('--left-hand-position', adjustValue('--left-hand-position', "reduce", 0.1));
+        document.documentElement.style.setProperty('--right-hand-position', adjustValue('--right-hand-position', "increase", -0.1));
+    } else {
+        document.documentElement.style.setProperty('--left-hand-position', adjustValue('--left-hand-position', "increase", 0.1));
+        document.documentElement.style.setProperty('--right-hand-position', adjustValue('--right-hand-position', "reduce", -0.1));
+    }
+
+}
+, 100)
+
+//on button functionality - attack to change the... squarity of the drawing field, mercy to clear it. act for funny fuckery and item to change the drawing mode
