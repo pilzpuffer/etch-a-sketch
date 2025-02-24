50940cc7c954bbb4d2eca4cc18a27536553acfc7
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-02-24 17:18:45 +0200

    Add base for the loading animation

---
+++ load.js
@@ -0,0 +1,12 @@
+heart = document.querySelector("#heart")
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    setInterval(function() {
+        
+    })
+
+    setTimeout(() => {
+        document.querySelector("#load").classList.toggle("invisible");
+    }, 500);
+});
