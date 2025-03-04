ad14eead133e173826b28944128134763fbdbeb5
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-03-04 14:39:44 +0200

    Change loading animation to allow sounds to play throughout the program

--- load.js
+++ load.js
@@ -1,23 +1,39 @@
-const beatAmount = 10;
-let beatCount = 0;
 let heartBeatDone = false;
+let heartBreakPlayed = false;
+
 
 window.addEventListener('load', () => {
-
-    const beatingMotion = setInterval(function() {
-        const heart = document.querySelector("#heart")
-            
-            heart.classList.toggle("beat");
-            beatCount++;
-
-            if (beatAmount === beatCount) {
-                heartBeatDone = true;
-                clearInterval(beatingMotion);
-                setTimeout(() => {
-                    document.querySelector("#load").classList.toggle("invisible");
-                }, 1000);
-            }
-    }, 180)
-
+    const startText = document.createElement("div");
+    startText.textContent = "[PRESS 2 OR ENTER TO START]";
+    startText.classList.add("start-text");
+    load.appendChild(startText);
     
+    const beatingMotion = setInterval(function() {
+        const heart = document.querySelector("#heart");
+        const heartBreak = document.querySelector("#battle-start-heartbreak");
+        const load = document.querySelector("#load");
+        heartBreak.volume = 0.15
+ 
+            heart.classList.toggle("beat");
+            
+            window.addEventListener("keydown", (event) => {
+                if (event.code === "Digit2" || event.code === "Enter") {
+                    start = true;
+
+                    if (start) {
+                        heartBeatDone = true;
+                        clearInterval(beatingMotion);
+                        heartBreak.play();
+                        heartBreakPlayed = true;
+
+                        if (heartBreakPlayed) {
+                            setTimeout(() => {
+                                startText.remove();
+                                load.remove();
+                            }, 1000);
+                        }
+                    }
+                }
+            }) 
+}, 180)
 });
